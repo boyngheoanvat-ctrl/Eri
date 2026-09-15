bba3fd563e19b5a0ee06658c07efd2f7d509d813
boyngheoanvat-ctrl <boyngheoanvat@gmail.com>
2026-09-15 10:23:35 +0700

    Delete il2cpp directory

--- il2cpp/il2cpp-class-internals.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <cstdint>
-struct Il2CppClass;
-struct Il2CppImage;
-struct Il2CppDomain;
--- il2cpp/il2cpp-object-internals.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "il2cpp-class-internals.h"
-struct Il2CppObject {
-    Il2CppClass* klass;
-    void* monitor;
-};
