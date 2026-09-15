59463635f24601b6ce144bb9ce6a6cb593389a43
boyngheoanvat-ctrl <boyngheoanvat@gmail.com>
2026-09-15 10:37:21 +0700

    Delete AutoDanceTweak directory

--- AutoDanceTweak/gc/gc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <functional>
-#include "../il2cpp/il2cpp-object-internals.h"
-namespace il2cpp { namespace gc {
-    class GarbageCollector {
-    public:
-        static void ForEachHeapObject(std::function<void(Il2CppObject*)> callback);
-    };
-}}
